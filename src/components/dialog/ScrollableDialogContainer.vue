5da48d921b35e6147e0ad09bbe59116f3a3618e5
seanZhenggg <s410585038@gmail.com>
2021-03-01 22:48:51 +0800

    [add] dialog scroll container component & antd style changes

---
+++ src/components/dialog/ScrollableDialogContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dialog-container__content beauty-scroll" :style="{'max-height': contentHeight}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollableDialogContainer',
+  props: {
+    contentHeight: {
+      type: String,
+      default: '55vh'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.dialog-container__content {
+  padding: 16px;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
